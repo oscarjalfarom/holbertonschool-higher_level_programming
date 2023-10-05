00057c899ac4f6689b183407d210048e16696d32
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-10-04 21:28:13 -0500

    Correcting errors in 0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
+# Oscar J Alfaro M
+
 
 def print_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
-        str_format = "{}".format(i)
-        print(str_format)
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))

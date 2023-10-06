2eedc700130e6cc637d6187e95ee0ca2a4c8b943
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-10-05 21:48:32 -0500

    Adding file 4-new_in_list.py. Contains a function to Replace an element in a copied list at a specific position.

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,8 +4,8 @@
 
 def new_in_list(my_list, idx, element):
     """replace an element in a list at a specific position without modifying the original list."""
-    cp_my_list = my_list
     if idx < 0 or idx > (len(my_list) - 1):
-        return (cp_my_list)
+        return (my_list[:])
+    cp_my_list = my_list[:]
     cp_my_list[idx] = element
     return (cp_my_list)

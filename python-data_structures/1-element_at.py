1093f424303606267eb5099d88c8cc5a4b871cd1
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-10-05 20:23:08 -0500

    Add files 1-element_at.py, correction

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 # 1-element_at.py
 # Oscar J Alfaro M <5826@holbertonschool.com>
-#!/usr/bin/python3
 
 def element_at(my_list, idx):
     """Retrive an element from a list."""
-    if idx < 0:
+    if idx < 0 or idx > (len(my_list) -1):
         return None
-    elif idx > (len(my_list)):
-        return None
-    else:
-        return my_list[idx]
+    return my_list[idx]

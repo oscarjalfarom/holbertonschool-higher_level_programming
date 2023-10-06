3a4fb0637e4b3bbbfd562af202593553116473c3
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-10-05 22:50:10 -0500

    Adding file 6-print_matrix_integer.py. Contains a function to Print a matrix of integers.

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+# Oscar J Alfaro M <5826@holbertonschool.com>
+
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers."""
+    for fila in matrix:
+        for i in fila:
+            print("{:d}".format(i), end=" ")
+        print()

61cfdd3b4156875440fde7a77dcb11b898eae644
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-10-13 21:29:45 -0500

    More Classes and Objects

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Oscar J Alfaro M <5826@holbertonschool.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass

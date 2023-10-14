c8c6048f827c4dbf2fe4cc1f475429f9c06d128e
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-10-13 20:16:51 -0500

    Starting Python - Classes and Objects

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Creating a class that represents a square
+The module will be used to create objects of class Square
+"""
+
+
+class Square:
+    """
+    The class that represents a square with a size, taking errors into account
+    """
+
+    def __init__(self, size=0):
+        """
+        Init for the square class oject
+
+        Args:
+            size (int): size of the square
+        """
+        if type(size) is int:
+            if (size >= 0):
+                self.__size = size
+            elif (size < 0):
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')

8b803f18c376503c72c769ec5e57f4c8b8c5170f
oscarjalfarom <dev.oscarjalfarom@gmail.com>
2023-11-04 22:03:20 -0500

    Starting Python - Inheritance, coorection task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-
-"""
-Defines class Square that inherits from Rectangle
-"""
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(__import__('9-rectangle').Rectangle):
-    """
-    class Square that inherits from Rectangle
-    """
+class Square(Rectangle):
+    """Represent a square."""
+
     def __init__(self, size):
-        """
-        Instantiation with size
-        """
-        if self.integer_validator("size", size):
-            self.__size = size
-            super().__init__(self.__size, self.__size)
+        """Initialize a new square.
 
-    def __str__(self):
+        Args:
+            size (int): The size of the new square.
         """
-        Returns rectangle description
-        """
-        return (f"[{__class__.__name__}] {self.__size}/{self.__size}")
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size

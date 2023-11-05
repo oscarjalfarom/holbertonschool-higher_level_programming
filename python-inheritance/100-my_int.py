29853d7e8150546a95d9119c8a6e3def60f11145
oscarjalfarom <dev.oscarjalfarom@gmail.com>
2023-11-04 21:58:48 -0500

    Starting Python - Inheritance

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Class that inherits from int and switches == and !=
+"""
+
+
+class MyInt(int):
+    """
+    Class that inherits from int
+    """
+    def __eq__(self, y):
+        """
+        Switch == for !=
+        """
+        return super().__ne__(y)
+
+    def __ne__(self, y):
+        """
+        Switch != for ==
+        """
+        return super().__eq__(y)
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Defines an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Empty Class
+    """
+    pass

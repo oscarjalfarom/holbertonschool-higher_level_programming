a57ce79aaffd1ee10d2419d8b49843f6cdcf9cbf
oscarjalfarom <dev.oscarjalfarom@gmail.com>
2023-11-06 15:46:12 -0500

    Python - Input/Output, correction 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-# 7-add_item.py
-# Oscar J Alfaro M <5826@holbertonschool.com>
-"""Add all arguments to a Python list and save them to a file."""
+"""
+    Script to save arguments to a file
+"""
+
 import sys
+import json
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    arguments = load_from_json_file("add_item.json")
+except Exception:
+    arguments = []
+
+i = 1
+while i < len(sys.argv):
+    arguments.append(sys.argv[i])
+    i += 1
+
+with open("add_item.json", encoding='utf-8', mode='w') as file:
+    file.write(json.dumps(arguments))

b7da6adb1af539449464f30660d2300f6541b283
oscarjalfarom <dev.oscarjalfarom@gmail.com>
2023-11-06 15:21:13 -0500

    Python - Input/Output, correction 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+
+"""
+Adds all arguments to a Python list, and then save them to a file
+"""
+
+
+import json
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+args_len = len(sys.argv)
+try:
+    data_structure = load_from_json_file('add_item.json')
+except:
+    data_structure = []
+for item in range(1, args_len):
+    data_structure.append(sys.argv[item])
+save_to_json_file(data_structure, 'add_item.json')
